b7450772d19f268b98b5277c489a377d50304297
Dylan <33008329+dylan0356@users.noreply.github.com>
2023-05-23 15:39:19 -0700

    Finished basic todo system

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,12 @@ function getTodos() {
   return prisma.todo.findMany()
 }
 
+async function toggleTodo(id: string, complete: boolean) {
+  "use server"
+
+  await prisma.todo.update({where: {id}, data: {complete}})
+}
+
 export default async function Home() {
 
   const todos = await getTodos()
@@ -20,7 +26,7 @@ export default async function Home() {
 
   <ul className='pl-4'>
     {todos.map(todo => (
-      <TodoItem key={todo.id} {...todo} />
+      <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
     ))}
   </ul>
   </>)
